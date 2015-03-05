5ffdfdca05e561d99c29e7c4febbb5ec867e12e7
gamer <gamer@gamerPC>
2015-03-05 22:16:27 +0100

    RADIUS constants updated

---
+++ JRaD-dev/src/main/com/punyal/jrad/core/radius/Message.java
@@ -0,0 +1,17 @@
+/**
+ * JRaD 2015
+ * @author Pablo Puñal Pereira <pablo@punyal.com>
+ * @version 0.1
+ */
+
+package com.punyal.jrad.core.radius;
+
+import java.util.logging.Logger;
+
+/**
+ * The class Message models the base class of all RADIUS messages.
+ */
+public abstract class Message {
+    protected final static Logger LOGGER = Logger.getLogger(Message.class.getCanonicalName());
+    
+}
--- JRaD-dev/src/main/com/punyal/jrad/core/radius/RADIUS.java
+++ JRaD-dev/src/main/com/punyal/jrad/core/radius/RADIUS.java
@@ -67,15 +67,15 @@ public class RADIUS {
         */
         public static Code valueOf(int value) {
             switch (value) {
-                   case 1:     return ACCESS_REQUEST;
-                   case 2:     return ACCESS_ACCEPT;
-                   case 3:     return ACCESS_REJECT;
-                   case 4:     return ACCOUNTING_REQUEST;
-                   case 5:     return ACCOUNTING_RESPONSE;
-                   case 11:    return ACCESS_CHALLENGE;
-                   case 12:    return STATUS_SERVER;
-                   case 13:    return STATUS_CLIENT;
-                   case 255:   return RESERVED;
+                   case   1: return ACCESS_REQUEST;
+                   case   2: return ACCESS_ACCEPT;
+                   case   3: return ACCESS_REJECT;
+                   case   4: return ACCOUNTING_REQUEST;
+                   case   5: return ACCOUNTING_RESPONSE;
+                   case  11: return ACCESS_CHALLENGE;
+                   case  12: return STATUS_SERVER;
+                   case  13: return STATUS_CLIENT;
+                   case 255: return RESERVED;
                    default: throw new IllegalArgumentException("Unknown RADIUS code "+value); 
             }
         }        
@@ -161,48 +161,47 @@ public class RADIUS {
         */
         public static Attribute valueOf(int value) {
             switch (value) {
-                   case  1:     return USER_NAME;
-                   case  2:     return USER_PASSWORD;
-                   case  3:     return CHAP_PASSWORD;
-                   case  4:     return NAS_IP_ADDRESS;
-                   case  5:     return NAS_PORT;
-                   case  6:     return SERVICE_TYPE;
-                   case  7:     return FRAMED_PROTOCOL;
-                   case  8:     return FRAMED_IP_ADDRESS;
-                   case  9:     return FRAMED_IP_NETMASK;
-                   case 10:     return FRAMED_ROUTING;
-                   case 11:     return FILTER_ID;
-                   case 12:     return FRAMED_MTU;
-                   case 13:     return FRAMED_COMPRESSION;
-                   case 14:     return LOGIN_IP_HOST;
-                   case 15:     return LOGIN_SERVICE;
-                   case 16:     return LOGIN_TCP_PORT;
-                   case 18:     return REPLY_MESSAGE;
-                   case 19:     return CALLBACK_NUMBER;
-                   case 20:     return CALLBACK_ID;
-                       
-                   case 22:     return FRAMED_ROUTE;
-                   case 23:     return FRAMED_IPX_NETWORK;
-                   case 24:     return STATE;
-                   case 25:     return CLASS;
-                   case 26:     return VENDOR_SPECIFIC;
-                   case 27:     return SESSION_TIMEOUT;
-                   case 28:     return IDLE_TIMEOUT;
-                   case 29:     return TERMINATION_ACTION;
-                   case 30:     return CALLED_STATION_ID;
-                   case 31:     return CALLING_STATION_ID;
-                   case 32:     return NAS_IDENTIFIER;
-                   case 33:     return PROXY_STATE;
-                   case 34:     return LOGIN_LAT_SERVICE;
-                   case 35:     return LOGIN_LAT_NODE;
-                   case 36:     return LOGIN_LAT_GROUP;
-                   case 37:     return FRAMED_APPLETALK_LINK;
-                   case 38:     return FRAMED_APPLETALK_NETWORK;
-                   case 39:     return FRAMED_APPLETALK_ZONE;
-                   case 60:     return CHAP_CHALLENGE;
-                   case 61:     return NAS_PORT_TYPE;
-                   case 62:     return PORT_LIMIT;
-                   case 63:     return LOGIN_LAT_PORT;
+                   case  1: return USER_NAME;
+                   case  2: return USER_PASSWORD;
+                   case  3: return CHAP_PASSWORD;
+                   case  4: return NAS_IP_ADDRESS;
+                   case  5: return NAS_PORT;
+                   case  6: return SERVICE_TYPE;
+                   case  7: return FRAMED_PROTOCOL;
+                   case  8: return FRAMED_IP_ADDRESS;
+                   case  9: return FRAMED_IP_NETMASK;
+                   case 10: return FRAMED_ROUTING;
+                   case 11: return FILTER_ID;
+                   case 12: return FRAMED_MTU;
+                   case 13: return FRAMED_COMPRESSION;
+                   case 14: return LOGIN_IP_HOST;
+                   case 15: return LOGIN_SERVICE;
+                   case 16: return LOGIN_TCP_PORT;
+                   case 18: return REPLY_MESSAGE;
+                   case 19: return CALLBACK_NUMBER;
+                   case 20: return CALLBACK_ID;
+                   case 22: return FRAMED_ROUTE;
+                   case 23: return FRAMED_IPX_NETWORK;
+                   case 24: return STATE;
+                   case 25: return CLASS;
+                   case 26: return VENDOR_SPECIFIC;
+                   case 27: return SESSION_TIMEOUT;
+                   case 28: return IDLE_TIMEOUT;
+                   case 29: return TERMINATION_ACTION;
+                   case 30: return CALLED_STATION_ID;
+                   case 31: return CALLING_STATION_ID;
+                   case 32: return NAS_IDENTIFIER;
+                   case 33: return PROXY_STATE;
+                   case 34: return LOGIN_LAT_SERVICE;
+                   case 35: return LOGIN_LAT_NODE;
+                   case 36: return LOGIN_LAT_GROUP;
+                   case 37: return FRAMED_APPLETALK_LINK;
+                   case 38: return FRAMED_APPLETALK_NETWORK;
+                   case 39: return FRAMED_APPLETALK_ZONE;
+                   case 60: return CHAP_CHALLENGE;
+                   case 61: return NAS_PORT_TYPE;
+                   case 62: return PORT_LIMIT;
+                   case 63: return LOGIN_LAT_PORT;
                    default: throw new IllegalArgumentException("Unknown RADIUS 2865 Attribute "+value); 
             }
         }        
